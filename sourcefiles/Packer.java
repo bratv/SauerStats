9aed7c91e0775ce44691c6acadc9108487f0b882
bratv <knackvurst@gmail.com>
2017-06-10 03:41:07 +0200

    Improved version
    Made an own TableModel by extending AbstractTableModel, and some changes that makes the sorting work better.

--- sourcefiles/Packer.java
+++ sourcefiles/Packer.java
@@ -1,5 +1,6 @@
 package pkg3;
 import java.util.ArrayList;
+
 public class Packer {
 	
 	private ArrayList<Byte> list;
@@ -43,66 +44,6 @@ public void pack(int n) {
 			
 		}
 		return bytez;
-	}
-	
-	public void pack2(int n) {
-		
-		if((n < 0) || (n >= (1<<21))) {
-			list.add((byte)(0x80 | (n & 0x7F)));
-			list.add((byte)(0x80 | ((n >> 7) & 0x7F)));
-			list.add((byte)(0x80 | ((n >> 14) & 0x7F)));
-			list.add((byte)(n >> 21));
-		}
-		else if(n < (1<<7)) {
-			list.add((byte)n);
-		}
-		else if(n < (1 << 14)) {
-			list.add((byte)(0x80 | (n & 0x7F)));
-			list.add((byte)(n >> 7));
-		}
-		else {
-			list.add((byte)(0x80 | (n & 0x7F)));
-			list.add((byte)(0x80 | ((n >> 7) & 0x7F)));
-			list.add((byte)(n >> 14));
-		}
-	}
-	
-	public int unpack() {
-		int n;
-		n = getInitial();
-		if(n == -128) {
-			n = get();
-			n |= get()<<8;
-			return n;
-		}
-		else if(n == -127) {
-			n = get();
-			n |= (get()<<8);
-			n |= (get()<<16);
-			n |= (get()<<24);
-			return n;
-		}
-		else {
-			return n;
-		}
-	}
-	
-	public int unpack2() {
-		int n;
-		n = get(); // getInitial()
-		if((n & 0x80) != 0) {
-			n += ((get() << 7) - (0x80));
-			if((n & (1<<14)) != 0) {
-				n += ((get() << 14) - (1 << 14));	
-			}
-			if((n & (1<<21)) != 0) {
-				n += ((get() << 21) - (1 << 21));
-			}
-			if((n & (1<<28)) != 0) {
-				n |= (-1<<28);
-			}
-		}
-		return n;
 	}
 	
 	private int getInitial() {
--- sourcefiles/Runner.java
+++ sourcefiles/Runner.java
@@ -5,26 +5,26 @@ import java.util.Vector;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
 public class Runner implements Runnable{
-	JTable table;
+	//JTable table;
 	Communicator com;
 	int[] message = {0, 1, -1};
 	Vector<String> columns;
 	Vector<Vector<Object>> list;
-	DefaultTableModel model;
-	TableRowSorter<DefaultTableModel> sorter;
+	MyTableModel model;
+	TableRowSorter<MyTableModel> sorter;
 	Thread thread;
 	private volatile boolean running;
 	
-	public Runner(JTable tab, Communicator c) {
-		table = tab;
+	public Runner(MyTableModel model, Communicator c) {
 		com = c;		
-		model = (DefaultTableModel) table.getModel();
-		sorter = new TableRowSorter<>(model);
-		table.setRowSorter(sorter);
-		sorter.setSortsOnUpdates(true);		
+		this.model = model;
+		sorter = new TableRowSorter<>(this.model);
+		//table.setRowSorter(sorter);
+		//sorter.setSortsOnUpdates(true);		
 		running = false;
 	}
 
@@ -35,13 +35,15 @@ public class Runner implements Runnable{
 			
 			SwingUtilities.invokeLater(new Runnable() {
 				public void run() {
-					for (int x = 0; x < list.size(); x++) {
-			            model.addRow(new Vector<Object>());
-			            for (int y = 0; y < table.getColumnCount(); y++) {			            	
-			                model.setValueAt(list.get(x).get(y), x, y);
-			            }
-					}
-					model.setRowCount(list.size());
+//					for (int x = 0; x < list.size(); x++) {
+//			            model.addRow(new Vector<Object>());
+//			            for (int y = 0; y < table.getColumnCount(); y++) {			            	
+//			                model.setValueAt(list.get(x).get(y), x, y);
+//			            }
+//					}
+//					model.setRowCount(list.size());
+					model.setData(list);
+					System.out.println("honk");
 
 				}
 			});
